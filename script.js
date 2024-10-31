fc2f4b4e78db1a5c56aec94a0243dc38b950af23
KENCH-N <151045596+KENCH-N@users.noreply.github.com>
2024-10-31 14:09:16 +0000

    sss Co-authored-by: River <River-Murray@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let name;
+
 function startGame() {
+    alert("Game Has Started");
+    name = prompt("What Is Your Name?");
+    alert("Welcome " +name+" to the Haunted All Saints High School!");
+    document.getElementById("para1").innerhtml = "You Are At The Entrance Of All Saints";
+}
+
+function choice1 {
     
 }
